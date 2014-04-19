5b5416d3f440e0f4ee839b9612e818fab2037c6c
Mindbergh <jlsqsd@gmail.com>
2014-04-19 10:35:00 +0800

    added in-code comments

--- cachematrix.R
+++ cachematrix.R
@@ -4,11 +4,14 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-		i <- NULL
+		## Return a list of functions that implement cached matrix
+		i <- NULL   
 		set <- function(data = NA, nrow = 1, ncol = 1, byrow = FALSE, dimnames = NULL) {
-			thismatrix <- matrix(NA,nrow,ncol,byrow,dimnames)
+			## the parameters are the same as build-in function matrix()
+			thismatrix <- matrix(datac,nrow,ncol,byrow,dimnames)
+			## use matrix() to create a matrix
 			x <<- thismatrix
-			i <<- NULL
+			i <<- NULL   ## reassign i as NULL as x may be changed
 		}
 		get <- function() x
 		setsolve <- function(inv) i <<- inv
@@ -22,12 +25,14 @@ makeCacheMatrix <- function(x = matrix()) {
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
 		i <- x$getsolve()
-		if(!is.null(m)) {
+		if(!is.null(i)) {
+				## if i is not NULL, it means that we have a cached solve, just return it
 				message("getting cached solve \n")
 				return (i)
 		}
+		## i is NULL, which means we don't have a cached solve, so compute it using solve()
 		data <- x$get()
 		i <- solve(data, ...)
-		x$setmean(i)
+		x$setsolve(i)
 		i
 }
